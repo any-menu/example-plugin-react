b929e9b3b6bac00fd030ecb195433846609b1a43
Linc <762699299@qq.com>
2026-06-17 00:44:03 +0800

    update: support app v1.2.0

--- src/index.ts
+++ src/index.ts
@@ -13,47 +13,48 @@
 // 功能: 可以将 CSS 内联到 JS 中，插件只需分发单个 .js 文件
 import cssText from './style.css?inline';
 
-import type { PluginInterface, PluginInterfaceCtx } from 'any-menu';
+import type { PluginAppCtx, PluginInterface, PluginRunCtx } from 'any-menu';
 
 import { createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import SubPanel from './SubPanel.jsx';
 
-let cache_ctx: PluginInterfaceCtx | undefined
+let app: PluginAppCtx
+let cache_el: HTMLElement | undefined
 
 export default class ExamplePluginSimple implements PluginInterface {
   metadata = {
     id: 'example-plugin-react',
     name: 'Example Plugin React',
-    version: '1.0.0',
-    min_app_version: '1.1.0',
+    version: '1.0.2',
+    min_app_version: '1.2.0',
     author: 'your-name',
     description: 'A minimal AnyMenu plugin template based on React.',
     icon: 'lucide-printer',
     css: cssText,
   };
 
-  onLoad(): void {
+  onLoad(app_: PluginAppCtx): void {
+    app = app_
     console.log('[ExamplePluginReact] Plugin loaded');
   }
 
   onUnload(): void {
-    if (cache_ctx) cache_ctx.api.unregisterSubPanel('example-plugin-react-panel')
+    app.api.unregisterSubPanel('example-plugin-react-panel')
     console.log('[ExamplePluginReact] Plugin unloaded');
   }
 
-  async run(ctx: PluginInterfaceCtx): Promise<void> {
+  async run(ctx: PluginRunCtx): Promise<void> {
     // 注册面板示例
-    if (!cache_ctx) {
-      cache_ctx = ctx
-      const newPanel = document.createElement('div');
-      ctx.api.registerSubPanel({
+    if (!cache_el) {
+      cache_el = document.createElement('div');
+      app.api.registerSubPanel({
           id: 'example-plugin-react-panel',
-          el: newPanel
+          el: cache_el
       })
 
       // 使用 React 渲染
-      const root = createRoot(newPanel);
+      const root = createRoot(cache_el);
       root.render(createElement(SubPanel));
       // root.render(<SubPanel />); // （在 .ts 中不能写 JSX，需用 createElement。但如果你的入口是 tsx，你可以用这种写法）
     }
@@ -62,16 +63,16 @@ export default class ExamplePluginSimple implements PluginInterface {
     const selected = ctx.env.selectedText;
     if (selected && selected.trim() !== '') {
       // 如果有选中文本，在其后追加问候
-      ctx.api.sendText(`${selected} — ExamplePluginReact!`);
+      app.api.sendText(`${selected} — ExamplePluginReact!`);
     } else {
       // 否则直接输出
-      // ctx.api.sendText('ExamplePluginReact!');
+      // app.api.sendText('ExamplePluginReact!');
 
       // 否则显示面板
-      ctx.api.hidePanel(['menu'])
-      ctx.api.showPanel(['example-plugin-react-panel'])
+      app.api.hidePanel(['menu'])
+      app.api.showPanel(['example-plugin-react-panel'])
     }
 
-    ctx.api.notify('ExamplePluginReact plugin executed ✅');
+    app.api.notify('ExamplePluginReact plugin executed ✅');
   }
 }
